2b8faca168ded9270479340aaab2a57f3a97ae27
wanglin <1013335014@qq.com>
2022-10-28 20:06:52 +0800

    完善文档&打包

---
+++ copy.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const path = require('path')
+
+const src = path.resolve(__dirname, './dist/index.html') 
+const dest = path.resolve(__dirname, './index.html') 
+
+if (fs.existsSync(dest)) {
+    fs.unlinkSync(dest)
+}
+
+if (fs.existsSync(src)) {
+    fs.copyFileSync(src, dest)
+    fs.unlinkSync(src)
+}
--- demo/vue.config.js
+++ demo/vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
+const isDev = process.env.NODE_ENV === 'development'
 module.exports = defineConfig({
+  publicPath: isDev ? '' : './dist',
+  outputDir: '../dist',
   transpileDependencies: true,
   lintOnSave: false
 })
